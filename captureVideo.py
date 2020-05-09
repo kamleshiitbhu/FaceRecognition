adff0163cc3d7ecdab8a2f45bb701c1c09d5dcb6
Kamlesh Kumar Jeengar <kamleshkumar356@gmail.com>
2020-05-09 19:58:42 +0530

    add face detection and draw rectanglr box

--- captureVideo.py
+++ captureVideo.py
@@ -1,4 +1,5 @@
 import numpy as np
+import face_recognition
 import cv2
 
 cap = cv2.VideoCapture(0)
@@ -9,9 +10,13 @@ while(True):
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    face_location = face_recognition.face_locations(frame)
+    for (top, right, bottom, left) in face_location:
+        #color for blue pass (255, 0, 0)
+        frame = cv2.rectangle(frame, (left, top), (right, bottom), color=(255, 255, 255), thickness = 2)
 
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
