4a63c6991ebf8c006a363e56ce09ef7d88e2f42c
Kamlesh Kumar Jeengar <kamleshkumar356@gmail.com>
2020-05-09 21:49:47 +0530

    fix image save only if face detected

--- cropface.py
+++ cropface.py
@@ -19,14 +19,17 @@ while(True):
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     face_location = face_recognition.face_locations(frame)
+    face_detected = False
     for (top, right, bottom, left) in face_location:
+        face_detected = True
         frame = frame[top :  bottom, left : right]     
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
-    file_name = '{}_{}.jpg'.format(name, count)
-    count += 1
-    cv2.imwrite(os.path.join(name_dir, file_name), frame)
+    if face_detected:
+        file_name = '{}_{}.jpg'.format(name, count)
+        count += 1
+        cv2.imwrite(os.path.join(name_dir, file_name), frame)
    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
