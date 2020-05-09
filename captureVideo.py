f3e7e4e43199084cfd233cec13ba0da6094ff5e9
Kamlesh Kumar Jeengar <kamleshkumar356@gmail.com>
2020-05-10 00:33:25 +0530

    version 1 of face detection

--- captureVideo.py
+++ captureVideo.py
@@ -1,8 +1,16 @@
 import numpy as np
 import face_recognition
 import cv2
+from generate_encoding import *
 
 cap = cv2.VideoCapture(0)
+
+folders = ['Kamlesh', 'Manoj', 'Shubham']
+
+encoding_dict = {}
+for folder in folders:
+    encode = getEncoding(folder)
+    encoding_dict[folder] = encode
 
 while(True):
     # Capture frame-by-frame
@@ -10,10 +18,18 @@ while(True):
 
     # Our operations on the frame come here
     #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    face_location = face_recognition.face_locations(frame)
-    for (top, right, bottom, left) in face_location:
+    face_locations = face_recognition.face_locations(frame)
+    face_encodings = face_recognition.face_encodings(frame)
+
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        if len(face_encoding) <= 0:
+            continue
         #color for blue pass (255, 0, 0)
         frame = cv2.rectangle(frame, (left, top), (right, bottom), color=(255, 255, 255), thickness = 2)
+        result = face_recognition.compare_faces(list(encoding_dict.values()), face_encoding)
+        name = (folders[np.argmax(result)])
+        cv2.putText(frame, name, (bottom, left), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0,255,0), 2)
+
 
     # Display the resulting frame
     cv2.imshow('frame', frame)
---
+++ generate_encoding.py
@@ -0,0 +1,21 @@
+import os
+import numpy as np
+import pandas as pd
+import sys
+import face_recognition
+import cv2
+
+
+def getEncoding(folder):
+    images = os.listdir(folder)
+
+    encoding = []
+    for image in images:
+        frame = cv2.imread(os.path.join(folder,image))
+        encode = face_recognition.face_encodings(frame)
+        if len(encode) > 0:
+            encoding.append(encode)
+
+    encoding = np.vstack(encoding)
+    encoding = np.average(encoding, axis = 0)
+    return encoding
