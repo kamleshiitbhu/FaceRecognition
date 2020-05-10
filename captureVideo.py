fc606d46fe45f416eba51c7c0108886b6847f74f
Kamlesh Kumar Jeengar <kamlesh.jeengar@mediakind.com>
2020-05-10 16:36:04 +0530

    fix flip issue on front camera

--- captureVideo.py
+++ captureVideo.py
@@ -14,6 +14,7 @@ while(True):
     ret, frame = cap.read()
     if ret == False:
         continue
+    frame = cv2.flip(frame, 1)
     small_frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
     # Our operations on the frame come here
     #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
